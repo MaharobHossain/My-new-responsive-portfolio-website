3c404b0b1950fbe1b49598e57243544ba9cd1d7b
Maharob Hossain <146083736+MaharobHossain@users.noreply.github.com>
2025-04-20 00:14:50 +0600

    scroll reveal and typed js

--- script/main.js
+++ script/main.js
@@ -44,3 +44,27 @@ navbar.classList.remove('active');
 
 };
 
+
+
+
+/*==========  scroll reveal  ========*/
+
+ScrollReveal({
+    distance: '80px', 
+    duration: 2000,
+    delay: 200,
+});
+ScrollReveal().reveal('.home-content, heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact from', { origin: 'buttom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+
+/*==========  typed js  ========*/
+
+const typed = new Typed('.multiple-text', {
+    strings: ['Fronted Developer', 'Web Designer', 'Influencer' ],
+    typeSpeed: 70,
+    backSpeed: 70,
+    backDelay: 1000,
+    loop: true,
+});
